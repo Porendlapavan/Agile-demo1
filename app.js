789d2babdb90995c570f53d3a7ab7e17a44214e8
Pavan Porendla <89634035+Porendlapavan@users.noreply.github.com>
2022-06-14 20:27:33 +0530

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-// main header navbar open on click
-function classToggle() {
-	const navs = document.querySelectorAll('.navbar-links')
-
-	navs.forEach(nav => nav.classList.add('navbar-links-active'));
-}
-
-document.querySelector('.toggle-buttons')
-	.addEventListener('click', classToggle);
